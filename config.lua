d1023877e8976348bde620f9265771388a983f25
Praryo <cyrrilericv@gmail.com>
2023-05-18 21:13:37 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.FloodStamps = {
+	[1] = 30, -- Level 1: 30 minutes by default.
+	[2] = 15, -- Level 2: 15 minutes by default.
+	[3] = 5, -- Level 3: 5 minutes by default.
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+lua54 "yes"
+
+fx_version 'cerulean'
+game 'gta5'
+
+description 'PYRP | Praryo Floods 1.0'
+version '1.0.0'
+
+client_scripts {
+	"client/*.lua",
+}
+
+server_scripts {
+	"server/*.lua",
+}
+
+shared_scripts {
+    'config.lua',
+}
+
+files {
+    'files/water1.xml',
+    'files/water2.xml',
+    'files/water3.xml',
+}
